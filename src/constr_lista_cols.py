81750f53aea2ad78fdaa4994ec89535246d87e69
florafirpo <firpoflorencia@gmail.com>
2025-11-12 10:16:30 -0300

    cols

--- src/constr_lista_cols.py
+++ src/constr_lista_cols.py
@@ -81,4 +81,125 @@ def get_cols_a_dropear(df: pd.DataFrame, columnas_base: list[str]) -> list[str]:
     
     return cols_a_dropear
 
+def cols_conteo_servicios_productos(df:pd.DataFrame)->Tuple[list,list,list,list,list,list,list,list]:
+    dict_prod_serv={
+    "master_visa_productos" : ["Master_msaldototal","Master_mconsumototal","Master_mpagado","Master_mlimitecompra",
+    "Visa_msaldototal","Visa_mconsumototal","Visa_mpagado","Visa_mlimitecompra"],
+
+    "cuentas_productos" : ['mcuenta_corriente','mcaja_ahorro','mcaja_ahorro_dolares','mcuentas_saldo',
+                        'mcuenta_corriente_adicional','mcaja_ahorro_adicional'],
+    "tarjetas_productos" :[c for c in df.columns if 'tarjeta' in c and c[0]=="m"],
+    "prestamos_productos" : [c for c in df.columns if 'prest' in c and c[0]=="m"],
+    "inversiones_productos" : ['mplazo_fijo_pesos','mplazo_fijo_dolares',
+                            'minversion1_pesos','minversion1_dolares','minversion2'],
+    "digitales_productos" : [c for c in df.columns if c[0]=="t"],
+    "servicios_productos" : ['mpagodeservicios','mpagomiscuentas','mcuenta_debitos_automaticos'
+                          ,'mforex_buy','mforex_sell','mtransferencias_recibidas','mtransferencias_emitidas',
+                          'mextraccion_autoservicio','mcheques_depositados','mcheques_emitidos','mcajeros_propios_descuentos'],
+    "seguros_productos" : [c for c in df.columns if 'segur' in c]}
+    return dict_prod_serv
+
+def cols_beneficios_presion_economica(df:pd.DataFrame):
+    ganancias_gastos={"ganancias" : [
+    # Ingresos por sueldo
+    "mpayroll","mpayroll2",
+    # Ahorro e inversiones (indican colchón financiero)
+    "mplazo_fijo_pesos","mplazo_fijo_dolares","minversion1_pesos","minversion1_dolares","minversion2",
+    # Plata que entra por transferencias
+    "mtransferencias_recibidas",
+    # Beneficios/descuentos (mejoran la situación neta)
+    "mcajeros_propios_descuentos","mtarjeta_visa_descuentos","mtarjeta_master_descuentos"],
+    
+    "gastos": [# Comisiones y costos directos
+    "mcomisiones","mcomisiones_mantenimiento","mcomisiones_otras",
+    # Débitos y pagos de servicios (egresos automáticos)
+    "mcuenta_debitos_automaticos","mpagodeservicios","mpagomiscuentas",
+    # Deuda / préstamos (presión financiera)
+    "mprestamos_personales","mprestamos_prendarios","mprestamos_hipotecarios","mpasivos_margen",
+    # Egresos por movimientos
+    "mtransferencias_emitidas","mextraccion_autoservicio",
+    "mcheques_emitidos","mcheques_depositados_rechazados","mcheques_emitidos_rechazados",
+    # Uso de cajeros/ATM (generalmente salida de plata)
+    "matm","matm_other"]
+    }
+    return ganancias_gastos
+
+
+def cols_a_dropear_variable_originales_o_corregidas(df: pd.DataFrame, a_eliminar : str = "originales") -> list[str]:
+    """
+    La uso para eliminar las variables originales o las que corregi con la media
+    Es mas para dejar las variables originales o las que corregi
+    """
+    logger.info("Comienzo de la seleccion de  originales o corregidas para eliminar")
+    logger.info(f"Se eligio {a_eliminar} para eliminar")
+    variables_corregidas = [c for c in df.columns if "_corregida" in c]
+    variables_originales = list(map(lambda x : x.replace("_corregida",""), variables_corregidas))
+    if a_eliminar =="originales":
+        cols_a_dropear = variables_originales
+    elif a_eliminar =="corregidas":
+        cols_a_dropear = variables_corregidas
+    logger.info(f"Fin de la seleccion de {a_eliminar} para eliminar")
+    return cols_a_dropear
+
+def cols_a_dropear_variable_originales_o_percentiles(df: pd.DataFrame, a_eliminar : str = "originales") -> list[str]:
+    """
+    La uso para eliminar las variables originales , las primeras 150 y pico, o las percentiles
+    Es mas para dejar las variables originales o las de percentiles
+    """
+    logger.info("Comienzo de la seleccion de persentiles o originales para eliminar")
+    logger.info(f"Se eligio {a_eliminar} para eliminar")
+    variables_percentiles = [c for c in df.columns if "_percentil" in c]
+    variables_originales = list(map(lambda x : x.replace("_percentil","") , variables_percentiles))
+    if a_eliminar =="originales":
+        cols_a_dropear = variables_originales
+    elif a_eliminar =="percentiles":
+        cols_a_dropear = variables_percentiles
+    logger.info(f"Fin de la seleccion de {a_eliminar} para eliminar")
+    return cols_a_dropear
+
+def cols_a_dropear_variable_entera(df: pd.DataFrame, columnas_base: list[str]) -> list[str]:
+    """
+    A partir de columnas base (ej: 'mrentabilidad'), detecta también
+    columnas derivadas como:
+      - mrentabilidad_lag_1, mrentabilidad_lag_2, ...
+      - delta_mrentabilidad
+      - max_mrentabilidad
+      - min_mrentabilidad
+      - slope_mrentabilidad
+    """
+    logger.info(f"Comienzo de la seleccion de la variable {columnas_base} ENTERA y todas sus derivadas para ELIMINAR")
+    
+
+    cols_a_dropear = list({c for c in df.columns for col_drop in columnas_base if c.startswith(col_drop)})
+    if len(cols_a_dropear)==0:
+        logger.warning(f"No se encontro ninguna de las columnas en el df")
+    logger.info(f"Fin del proceso de la seleccion de la variable entera y todas sus derivadas para ELIMINAR")
+    
+    return cols_a_dropear
+
+
+def cols_a_dropear_variable_por_feat(df: pd.DataFrame, columnas_variables: list[str] , columnas_features:list[str]) -> list[str]:
+    logger.info(f"Comienzo de la seleccion de las variables {columnas_features} a elimnar por features {columnas_features}")
+    cols_a_dropear =[]
+    for cv in columnas_variables:
+        for cf in columnas_features:
+            cols_a_dropear.append(cv+cf)
+    cols_a_dropear = [c for c in cols_a_dropear if c in df.columns]
+    logger.info(f"Fin de la seleccion de las variables a elimnar por feature {cols_a_dropear}")
+
+    return cols_a_dropear
+
+
+
+
+def contrs_cols_dropear_feat_imp(df:pd.DataFrame , file:str , threshold:float)->list[str]:
+    logger.info(f"Comienzo de la seleccion de columnas a dropear")
+    importance_df=pd.read_excel(file)
+    f = importance_df["importance_%"]<=threshold
+    cols_menos_importantes=list(importance_df.loc[f,'feature'].unique())
+    cols_no_dropear=["foto_mes","numero_de_cliente"]
+    cols_dropear=[c for c in  cols_menos_importantes if c not in cols_no_dropear]
+    logger.info(f"Fin de la seleccion de columnas a dropear. Se eliminaran {len(cols_dropear)} columnas")
+
+    return cols_dropear
 
--- src_feateng/feat_eng_1.py
+++ src_feateng/feat_eng_1.py
@@ -4,7 +4,7 @@ import logging
 import json
 from src.config import *
 from src.configuracion_inicial import creacion_df_small
-from src.constr_lista_cols import contruccion_cols
+from src.constr_lista_cols import contruccion_cols, cols_conteo_servicios_productos, cols_beneficios_presion_economica
 from src.feature_engineering import feature_engineering_lag,feature_engineering_delta,feature_engineering_max_min,feature_engineering_ratio,feature_engineering_linreg,feature_engineering_drop_cols, feature_engineering_percentil,feature_engineering_drop_meses, suma_de_prod_servs, suma_ganancias_gastos, ratios_ganancia_gastos
 
 ## ---------------------------------------------------------Configuraciones Iniciales -------------------------------
@@ -27,15 +27,9 @@ def lanzar_feat_eng(fecha:str ,n_fe:int , proceso_ppal:str):
 
         #DROPEO INICIAL DE MESES
 
-    meses_a_dropear=[201901, 201902, 201903, 201904, 201905, 201906, 201907, 201908, 201909, 201910, 201911, 201912, 202006]
+    meses_a_dropear=[201901, 201902, 201903, 201904, 201905, 201906, 201907, 201908, 201909]
     feature_engineering_drop_meses(meses_a_dropear,"df_completo","df_completo")
     
-
-    # CORRECCION DE VARIABLES POR MES POR MEDIA
-    # df_completo_chiquito=creacion_df_small("df_completo")
-    # variable_meses_dict={"mrentabilidad":"(202106,202105)"}
-    # feature_engineering_correccion_variables_por_mes_por_media(df_completo_chiquito,variable_meses_dict)
-
     # SERVICIOS Y PRODUCTOS
     df_completo_chiquito=creacion_df_small("df_completo")
     dict_prod_serv=cols_conteo_servicios_productos(df_completo_chiquito)
